6b48877c4d8387cd953ddf8466692fbf455014c3
karol-broda <karol.broda@titanom.com>
2023-11-18 18:33:43 +0100

    Refactor ping monitoring code and switch to PostgreSQL database

--- pyflynet.py
+++ pyflynet.py
@@ -1,10 +1,12 @@
 import argparse
 import time
+import os
+import ping3
+import psycopg2
+import logging
 from typing import Union, Tuple, Optional
 
-import ping3
-import sqlite3
-
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def ping(ip_addr: str, timeout: int) -> Tuple[Optional[float], Optional[str]]:
     try:
@@ -16,91 +18,84 @@ def ping(ip_addr: str, timeout: int) -> Tuple[Optional[float], Optional[str]]:
     except Exception as e:
         return None, str(e)
 
+def create_ping_result_table():
+    conn = None
+    try:
+        conn = psycopg2.connect(
+            user="postgres",
+            password=os.environ.get("DATABASE_PASS"),
+            host="db.beogantahxdrfrqhnqjn.supabase.co",
+            port=5432,
+            database="postgres"
+        )
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS ping_results (
+                id SERIAL PRIMARY KEY,
+                timestamp FLOAT,
+                latency FLOAT,
+                error TEXT
+            )
+            """
+        )
+        conn.commit()
+    except Exception as e:
+        logging.error("Error creating table: %s", e)
+    finally:
+        if conn is not None:
+            conn.close()
 
-def create_ping_result_table(db_name: str) -> None:
-    conn = sqlite3.connect(db_name)
-    cursor = conn.cursor()
-    cursor.execute(
-        """
-    CREATE TABLE IF NOT EXISTS ping_results (
-        timestamp FLOAT,
-        latency FLOAT,
-        error TEXT
-    )
-    """
-    )
-    conn.commit()
-    conn.close()
 
-
-def insert_ping_result(
-    db_name: str, timestamp: float, latency: Union[float, None], error: Union[str, None]
-) -> None:
-    conn = sqlite3.connect(db_name)
-    cursor = conn.cursor()
-    cursor.execute(
-        """
-    INSERT INTO ping_results (timestamp, latency, error) VALUES (?, ?, ?)
-    """,
-        (timestamp, latency, error),
-    )
-    conn.commit()
-    conn.close()
+def insert_ping_result(timestamp: float, latency: Union[float, None], error: Union[str, None]) -> None:
+    conn = None
+    try:
+        conn = psycopg2.connect(
+            user="postgres",
+            password=os.environ.get("DATABASE_PASS"),
+            host="db.beogantahxdrfrqhnqjn.supabase.co",
+            port=5432,
+            database="postgres"
+        )
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            INSERT INTO ping_results (timestamp, latency, error) VALUES (%s, %s, %s)
+            """,
+            (timestamp, latency, error),
+        )
+        conn.commit()
+    except Exception as e:
+        logging.error("Error inserting ping result: %s", e)
+    finally:
+        if conn is not None:
+            conn.close()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="pyflynet")
 
-    parser.add_argument(
-        "--ip", metavar="ip", type=str, default="8.8.8.8", help="The IP address to ping"
-    )
-    parser.add_argument(
-        "--db",
-        metavar="db",
-        type=str,
-        default="pyflynet.db",
-        help="The name of the SQLite database to which to save the ping results",
-    )
-    parser.add_argument(
-        "--every",
-        metavar="every",
-        type=int,
-        default=5,
-        help="The delay between pings",
-    )
-    parser.add_argument(
-        "--timeout",
-        metavar="timeout",
-        type=int,
-        default=4,
-        help="The timeout for each ping in seconds",
-    )
+    parser.add_argument("--ip", metavar="ip", type=str, default="8.8.8.8", help="The IP address to ping")
+    parser.add_argument("--every", metavar="every", type=int, default=3, help="The delay between pings")
+    parser.add_argument("--timeout", metavar="timeout", type=int, default=4, help="The timeout for each ping in seconds")
 
     args = parser.parse_args()
 
-    db = args.db
     ip = args.ip
     every = args.every
     timeout = args.timeout
-    print(
-        f"Pinging {ip} every {every} seconds with a timeout of {timeout} seconds and saving results to {args.db}"
-    )
+    logging.info(f"Pinging {ip} every {every} seconds with a timeout of {timeout} seconds")
 
-    create_ping_result_table(db)
+    create_ping_result_table()
 
-    last_ping_time = None
+    try:
+        while True:
+            start_time = time.time()
+            latency, error = ping(ip, timeout)
+            insert_ping_result(start_time, latency, error)
+            logging.info(f"{start_time}: Latency: {latency}, Error: {error}")
 
-    while True:
-        last_ping_time = time.time()
-        latency, error = ping(ip, timeout)
-        ping_end = time.time()
-
-        next_time = time.time()
-        insert_ping_result(db, last_ping_time, latency, error)
-        print(f"{last_ping_time}: Latency: {latency}, Error: {error}")
-
-        current_time = time.time()
-        elapsed_time = next_time - current_time
-        sleep_time = max(every - elapsed_time, 0)
-
-        time.sleep(sleep_time)
+            sleep_time = max(every - (time.time() - start_time), 0)
+            time.sleep(sleep_time)
+    except KeyboardInterrupt:
+        logging.info("Ping monitoring stopped by user.")
